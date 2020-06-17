817973aeed0126b732a5db0f42b9ba102b06752a
Adam Workman <adammworkman25@gmail.com>
2020-06-17 17:00:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def @batch_badge_creator(attendees)
+def @batch_badge_creator,(attendees)
   attendees.collect do |attendee|
     "Hello, my name is #{attendee}."
   end
