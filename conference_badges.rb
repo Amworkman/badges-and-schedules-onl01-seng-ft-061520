1e1558b5c1e9116bce59762f4e28cb37de6b8c1e
Adam Workman <adammworkman25@gmail.com>
2020-06-17 16:33:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   room = 1 
-  attendees.collect do |attendee|
+  attendees.map do |attendee|
     "Hello, #{attendee}! You'll be assigned to room #{room}!" 
     room += 1 
 end
