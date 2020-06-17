b1530b66f322f2e04e16f3f7312154f2ecdac687
Adam Workman <adammworkman25@gmail.com>
2020-06-17 16:59:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,13 +2,13 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def @batch_badge_creator(attendees)
   attendees.collect do |attendee|
     "Hello, my name is #{attendee}."
   end
 end
 
-def assign_rooms(attendees)
+def @assign_rooms(attendees)
    attendees.each_with_index.collect do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
    end
@@ -16,4 +16,6 @@ def assign_rooms(attendees)
 end
 
 def printer
+  puts @batch_badge_creator
+  puts @assign_rooms
 end
