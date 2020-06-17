0cca280bb3e1af5537096836e97a84fe3f525d81
Adam Workman <adammworkman25@gmail.com>
2020-06-17 17:09:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,6 @@ def assign_rooms(attendees)
    attendees.each_with_index.collect do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
    end
-
 end
 
 def printer
